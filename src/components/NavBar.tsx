ae2039b52ea2e0beb46f2d290c85373f226646bc
JesseyStend <thejes@hotmail.nl>
2024-03-11 11:51:38 +0100

    T3 init

--- src/components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-const NavBar = () => {
-  return (
-    <nav className="px-4">
-      <h2>text</h2>
-    </nav>
-  );
-};
-
-export default NavBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,14 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
-const config: Config = {
-  darkMode: ["class", '[data-mode="dark"]'],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./src/**/*.tsx"],
   theme: {
-    colors: {
-      colors: {
-        text: "var(--text)",
-        background: "var(--background)",
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        accent: "var(--accent)",
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
       },
     },
-    
-  },
-  fontSize: {
-    sm: "0.750rem",
-    base: "1rem",
-    xl: "1.333rem",
-    "2xl": "1.777rem",
-    "3xl": "2.369rem",
-    "4xl": "3.158rem",
-    "5xl": "4.210rem",
-  },
-  fontFamily: {
-    heading: "Poppins",
-    body: "Poppins",
-  },
-  fontWeight: {
-    normal: "400",
-    bold: "700",
   },
   plugins: [],
-};
-export default config;
+} satisfies Config;
